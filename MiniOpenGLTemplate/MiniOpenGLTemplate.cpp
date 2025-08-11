7248a7822831a2a339700952452fcfe8b18304e6
andrea bellizzi <bellizzian@gmail.com>
2025-08-11 21:49:34 +0200

    first commit

--- MiniOpenGLTemplate/MiniOpenGLTemplate.cpp
+++ MiniOpenGLTemplate/MiniOpenGLTemplate.cpp
@@ -1,12 +1,64 @@
 ﻿// MiniOpenGLTemplate.cpp : Defines the entry point for the application.
 //
-
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include "MiniOpenGLTemplate.h"
 
-using namespace std;
+static unsigned int WINDOW_WIDTH  = 800;
+static unsigned int WINDOW_HEIGHT = 600;
+static float deltaTime = 0.0f; // time between current frame and last frame
+static float lastFrame = 0.0f; // time of last frame
+static float currentFrame = 0.0f; // time of current frame
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "LearnOpenGl", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // init glad before any opengl
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    while (!glfwWindowShouldClose(window)) {
+        currentFrame = glfwGetTime();
+        if (deltaTime >= 1.0f / 60.0f) {
+            // check input
+            
+            // rendering
+
+            // set clear color (it is the background)
+            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the color and depth buffer
+            
+            glfwPollEvents();
+            glfwSwapBuffers(window);
+            deltaTime = deltaTime - 1.0f / 60.0f;
+        }
+        deltaTime += currentFrame - lastFrame;
+        lastFrame = currentFrame;
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+	WINDOW_WIDTH = width;
+	WINDOW_HEIGHT = height;
 }
